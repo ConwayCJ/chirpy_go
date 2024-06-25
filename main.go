ef4c050b6f6a178a9acfbf0842b2781aa8ec9f2f
conwaycj <christopherjconway07@gmail.com>
2024-06-24 20:03:15 -0700

    apply get methods

--- main.go
+++ main.go
@@ -3,18 +3,56 @@ package main
 import (
 	"log"
 	"net/http"
+	"strconv"
 )
 
+type apiConfig struct {
+	fileserverHits int
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits++
+		next.ServeHTTP(w, r)
+	})
+}
+
 func main() {
+
+	cfg := &apiConfig{}
+
 	const port = "8080"
+	filepathRoot := "."
 
 	mux := http.NewServeMux()
-
+	mux.Handle("/app/*", cfg.middlewareMetricsInc(http.StripPrefix("/app/", http.FileServer(http.Dir(filepathRoot)))))
 	srv := &http.Server{
 		Addr:    ":" + port,
 		Handler: mux,
 	}
 
+	mux.HandleFunc("GET /healthz", srvHealth)
+	mux.HandleFunc("/reset", cfg.srvResetCount)
+	mux.HandleFunc("GET /metrics", cfg.srvMetrics)
+
 	log.Printf("Serving on port: %s\n", port)
 	log.Fatal(srv.ListenAndServe())
+}
+
+func srvHealth(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
+}
+
+func (cfg *apiConfig) srvMetrics(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	numHits := strconv.Itoa(cfg.fileserverHits)
+	w.Write([]byte("Hits: " + numHits))
+}
+
+func (cfg *apiConfig) srvResetCount(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits = 0
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
 }
